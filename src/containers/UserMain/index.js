3bb01cca1b0cececb44b1c669e324c5cd78f5703
sjiwoo <undagada@gmail.com>
2020-06-09 15:22:10 -0400

    beam request changes

--- src/containers/UserMain/index.js
+++ src/containers/UserMain/index.js
@@ -1,20 +1,11 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-
-import Button from '../../components/Button';
 import Card from '../../components/Card';
-import InputField from '../../components/InputField';
 import Main from '../../components/Main';
 import Paragraph from '../../components/Paragraph';
 import Row from '../../components/Row';
-import Stack from '../../components/Stack';
 import LoginForm from '../../components/LoginForm';
-import HerokuText from '../../HerokuText';
 import MDAHeader from '../../components/MDAHeader';
 
-
-let text = "caleb";
-              
 export default () => (
   <Main style={{ justifyContent: 'center', alignItems: 'center' }}>
     <MDAHeader/>
