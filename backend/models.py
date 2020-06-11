9b4102bfa6b5f2e6ccf42ca26d6a77148df7e606
Robert Yale Home <rcy6@duke.edu>
2020-06-11 02:52:48 -0400

    added login tokens

--- backend/models.py
+++ backend/models.py
@@ -1,4 +1,4 @@
-from main import db, bcrypt, login
+from main import db, bcrypt
 from sqlalchemy.dialects.postgresql import JSON
 from flask_login import UserMixin
 
@@ -51,7 +51,22 @@ class Calendar(db.Model):
     def __repr__(self):
         return "<Calendar(username=%s)>" % (self.username)
 
-@login.user_loader
-def load_user(username):
-    print(Users.query.filter_by(username=username).first())
-    return Users.query.filter_by(username=username).first()
+class TokenBlacklist(db.Model):
+    """Model for the stations table"""
+    __tablename__ = 'TokenBlacklist'
+    id = db.Column(db.Integer, primary_key=True)
+    jti = db.Column(db.String(36), nullable=False)
+    token_type = db.Column(db.String(10), nullable=False)
+    user_identity = db.Column(db.String(50), nullable=False)
+    revoked = db.Column(db.Boolean, nullable=False)
+    expires = db.Column(db.DateTime, nullable=False)
+
+    def to_dict(self):
+        return {
+            'token_id': self.id,
+            'jti': self.jti,
+            'token_type': self.token_type,
+            'user_identity': self.user_identity,
+            'revoked': self.revoked,
+            'expires': self.expires
+        }
