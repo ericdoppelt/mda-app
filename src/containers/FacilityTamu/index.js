57942452a2b9bf4c789a58337e2957a531b7a9f9
Eric Doppelt <ead45@duke.edu>
2020-06-16 14:03:38 -0400

    facility tamu name change

--- src/containers/FacilityTamu/index.js
+++ src/containers/FacilityTamu/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 
-import Card from '../../components/Card';
 import Image from '../../components/Image';
 import Main from '../../components/Main';
 import MDAHeader from '../../components/MDAHeader';
