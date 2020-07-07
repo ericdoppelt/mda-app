9aad4523c787aac5bed80464ecfd2ece975f0ef0
sjiwoo <undagada@gmail.com>
2020-07-07 13:32:23 -0400

    personal calendar

--- src/containers/User/UserProfile/index.js
+++ src/containers/User/UserProfile/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { createContext } from "react";
 import Row from '../../../components/UIzard/Row';
 import Card from '../../../components/UIzard/Card';
 import MDAHeader2 from '../../../components/Custom/MDAHeader2';
@@ -6,8 +7,15 @@ import * as Constants from '../../../constants'
 import { ThemeProvider } from '@material-ui/core/styles';
 import UserProfile from '../../../components/Custom/UserProfile';
 import Calendar from '../../../components/Custom/Calendar/Calendar';
+import { observer } from "mobx-react"
+import { observable } from "mobx"
+import UserProfileStorage from '../../../stores/UserProfileStore';
+
+const samp = observable({ text: "John" })
+const UserProfileStore = createContext(UserProfileStorage);
 
 export default function Home() {
+
 
   const classes = Constants.useStyles();
   return (
@@ -18,10 +26,12 @@ export default function Home() {
           <div className={classes.appBarSpacer} />
         <Row style={{ justifyContent: 'center', minWidth: '50px', minHeight: '50px' }}>
           <Card style={{ justifyContent: 'center', minWidth: '50px', minHeight: '570px', width: '720px', flexGrow: '0' }}>
-            <Row style={{ justifyContent: 'center', flexGrow: '0', minWidth: '50px', minHeight: '50px' }}>
-              <UserProfile/>
-            </Row>
-            <Calendar/>
+            <UserProfileStore.Provider>
+              <Row style={{ justifyContent: 'center', flexGrow: '0', minWidth: '50px', minHeight: '50px' }}>
+                <UserProfile samp={samp}/>
+              </Row>
+              <Calendar personal={true}/>
+            </UserProfileStore.Provider>
           </Card>
         </Row>
         </main>
