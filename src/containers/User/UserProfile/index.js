1bdc7da4435a866b5b1a83a1e8e9e72419a4985b
Caleb Rummel <calebwrummel@gmail.com>
2020-07-13 13:35:38 -0400

    Remove todolist

--- src/containers/User/UserProfile/index.js
+++ src/containers/User/UserProfile/index.js
@@ -40,7 +40,7 @@ export default function Home() {
           <Card style={{ justifyContent: 'center', minWidth: '50px', minHeight: '570px', width: '720px', flexGrow: '0' }}>
           <h1>To-do List</h1>
           <Row style={{ justifyContent: 'center', flexGrow: '0', minWidth: '50px', minHeight: '50px' }}>
-          <UsertodoList/>
+          Under development
           </Row>
           </Card>
           </Row>
