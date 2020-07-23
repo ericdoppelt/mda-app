30bece346b58fe17e9e9bd0c4fefc22ec1d045e3
Robert Yale Home <rcy6@duke.edu>
2020-07-22 20:48:33 -0400

    swapped energy and beams

--- backend/routes/beams.py
+++ backend/routes/beams.py
@@ -13,10 +13,10 @@ def beams():
     facility = Organization.query.filter_by(abbrv=req['facility']).one()
     beams = Beams.query.filter_by(org_id=facility.id).all()
     for beam in beams:
-        if beam.ion in myList:
-            myList[beam.ion].append(beam.amev)
+        if beam.amev in myList:
+            myList[beam.amev].append(beam.ion)
         else:
-            myList[beam.ion] = [beam.amev]
+            myList[beam.amev] = [beam.ion]
         # beam_info = {'ion': beam.ion,
         # 'mass': beam.mass, 'amev': beam.amev, 'max_energy': beam.max_energy,
         # 'max_energy_units': beam.max_energy_units, 'let': beam.let, 'let_units': beam.let_units,
